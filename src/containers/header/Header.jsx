368974ca36e9348f29cbb08a8ffd71494c5abaef
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-24 12:34:58 +0100

    Header component laid out and styled
    Be advised. This is not the final state of this component as I may decide at anytime to make changes or add new features. Cheers!

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { images } from '../../constants'
+import { SubHeading } from "../../components"
 import "./header.css"
 
 const Header = () => {
   return (
-    <div>
-      Header    
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase the new Flavour"/>
+        <h1 className="app__header-h1">The Key to Fine Dining</h1>
+        <p className="p__opensans" style={{margin: "2rem 0"}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+        <button type='button' className='custom__button'>Explore Menu</button>
+      </div>
+
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt="header image" />
+      </div>
     </div>
   )
 }
