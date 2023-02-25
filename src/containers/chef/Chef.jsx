d91a7bd95cdb4cfdea57eb64151786139854bdbe
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-25 18:41:56 +0100

    Chef container laid out and styled

--- src/containers/chef/Chef.jsx
+++ src/containers/chef/Chef.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { images } from "../../constants"
+import { SubHeading } from "../../components"
 import "./chef.css"
 
 const Chef = () => {
   return (
-    <div>
-      Chef
+    <div className='app__bg app__wrapper section__padding'>
+      <div className="app__wrapper_img app__wrapper_img-reverse">
+        <img src={images.chef} alt="Our Chef" />
+      </div>
+      <div className="app__wrapper_info">
+        <SubHeading title="Chef's Word" />
+        <h1 className="headtext__cormorant">What we believe in</h1>
+        <div className="app__chef-content">
+          <div className="app__chef-content_quote">
+            <p className="p__opensans"><img src={images.quote} alt="quote" />Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit. auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc.</p>
+          </div>
+        </div>
+        <div className="app__chef-sign">
+          <p>Kevin Luo</p>
+          <p className="p__opensans">Chef and Founder</p>
+          <img src={images.sign} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
