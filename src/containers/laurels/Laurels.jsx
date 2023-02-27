c94f509afe45f1ed99e6e2a5c3320cf0ed00ab47
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-27 14:22:19 +0100

    Laurels container initialized and implemented

--- src/containers/laurels/Laurels.jsx
+++ src/containers/laurels/Laurels.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import "./laurels.css"
+import { images, data } from "../../constants"
+import { SubHeading, Award } from '../../components'
+import { nanoid } from 'nanoid'
 
 const Laurels = () => {
+
+  const awards = data.awards.map(awardObj => (
+    <Award 
+      key={nanoid()}
+      title={awardObj.title}
+      image={awardObj.imgUrl}
+      subTitle={awardObj.subtitle}
+    />
+  ))
+
   return (
-    <div>
-      Laurel
+    <div className="app__laurels app__bg app__wrapper section__padding" id='awards'>
+      <div className="app__laurels-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <div className="app__laurels-content">
+        <div className="app__laurels-content_heading">
+          <SubHeading title="Awards & recognition"/>
+          <h1 className="headtext__cormorant">Our Laurels</h1>
+        </div>
+        <div className="app__laurels-content_awards">
+          {awards}
+        </div>
+      </div>
+      <div className="app__laurels-image">
+          <img src={images.laurels} alt="laurel food" />
+      </div>
     </div>
   )
 }
