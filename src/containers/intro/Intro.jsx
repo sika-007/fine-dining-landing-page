837de21db1da4f0957d7d0fdf225adb3ba7e551e
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-27 14:19:54 +0100

    Intro section laid out and styled

--- src/containers/intro/Intro.jsx
+++ src/containers/intro/Intro.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs"
+import { meal } from "../../constants"
 import "./intro.css"
 
 const Intro = () => {
+
   return (
-    <div>
-      Intro
+    <div className='app__video'>
+      <video
+        src={meal}
+        autoPlay = {true} 
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      ></video>
+      <div className="app__video-overlay flex__center">
+      </div>
     </div>
   )
 }
