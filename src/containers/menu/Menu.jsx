4304008ff3b40a181cb7800fad791252322a1723
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-28 15:39:24 +0100

    bug fixes and typo correction

--- src/containers/menu/Menu.jsx
+++ src/containers/menu/Menu.jsx
@@ -14,14 +14,14 @@ const Menu = () => {
   ))
 
   return (
-    <div className='app__menu flex__center section__padding' id='home'>
+    <div className='app__menu flex__center section__padding' id='menu'>
       <div className="app__menu-title">
         <SubHeading title="Menu that fits you palatte"/>
         <h1 className="headtext__cormorant">Today's Special</h1>
       </div>
       <div className="app__menu-menu">
         <div className="app__menu-menu_wine flex__center">
-          <p className='app__menu-menu_heading'>Wine and Beer</p>
+          <p className='app__menu-menu_heading'>Wine & Beer</p>
           <div className='app__menu-menu_items'>
             {wineElements}
           </div>
