0bf0e25cf13b2bc3bc0c00e37e78bdab998902ae
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-28 15:36:28 +0100

    navbar bug fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -41,20 +41,20 @@ const Navbar = () => {
         {toggleMenu && (
           <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
             <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)}/>
-            <ul className='app__navbar-smallscreen_links'>
-              <li className='p__opensans'>
+            <ul className='app__navbar-smallscreen_links' onClick={() => {setToggleMenu(false)}}>
+              <li className='p__opensans' onClick={() => {}}>
                 <a href="#home">Home</a>
               </li>
-              <li className='p__opensans'>
+              <li className='p__opensans' onClick={() => {}}>
                 <a href="#about">About</a>
               </li>
-              <li className='p__opensans'>
+              <li className='p__opensans' onClick={() => {}}>
                 <a href="#menu">Menu</a>
               </li>
-              <li className='p__opensans'>
+              <li className='p__opensans' onClick={() => {}}>
                 <a href="#awards">Awards</a>
               </li>
-              <li className='p__opensans'>
+              <li className='p__opensans' onClick={() => {}}>
                 <a href="#contact">Contact</a>
               </li>
             </ul>
